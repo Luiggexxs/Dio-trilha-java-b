4497c498cc7e9c87485833b6e7a2397c3922d161
Luiggexxs <luigge1106@gmail.com>
2023-10-06 00:36:48 -0300

    Add for each

--- Estrutura-rep/src/ExemploFor.java
+++ Estrutura-rep/src/ExemploFor.java
@@ -2,8 +2,13 @@ public class ExemploFor {
 public static void main(String[] args) {
 	String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
-	for (int x=0; x<alunos.length; x++) {
-		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	/*for(int x = 0; x <alunos.length; x++){
+		System.out.println("O aluno no indice x=" + x + " É " + alunos[x]);
+	}*/
+
+	/* Usando FOR EACH especifico para listas */
+	for(String aluno : alunos){
+		System.out.println("O nome do do aluno é " + aluno);
 	}
 
 }
